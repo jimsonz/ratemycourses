c9c99497786ec7753cb0ca7fa708d8982ea58075
jimsonz <jimson0321@gmail.com>
2013-04-01 23:01:52 -0400

    Revert "Revert "Revert "added current project"""
    This reverts commit 7e739be7feb45f412017adbfe429ea7787a01f2c.

--- RateMyCourses/src/com/example/ratemycourses/course/CourseRatingView.java
+++
@@ -1,7 +0,0 @@
-package com.example.ratemycourses.course;
-
-import android.app.Activity;
-
-public class CourseRatingView extends Activity {
-
-}
--- RateMyCourses/src/com/example/ratemycourses/listview/ProgramList.java
+++
@@ -1,5 +0,0 @@
-package com.example.ratemycourses.listview;
-
-public class ProgramList {
-
-}
